f9c57eec95c19f63825995d5944362d6b0ca992d
Miłosz Bieniek <bieniek.milosz0@gmail.com>
2022-05-31 16:13:11 +0200

    Time: 4 ms (40.95%), Space: 8.8 MB (73.58%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -3,7 +3,7 @@ public:
     int removeElement(vector<int>& nums, int val) {
       int k {0};
       
-      for(int i{0}; i < nums.size(); ++i){
+      for(auto i{0}; i < nums.size(); ++i){
         if(nums[i] == val){
           nums.erase(nums.begin() + i);
           --i;
