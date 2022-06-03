d637d3e1faaf1355188a5c4a6adb7eb927a561ec
Miłosz Bieniek <bieniek.milosz0@gmail.com>
2022-06-03 15:42:54 +0200

    Time: 0 ms (100.00%), Space: 6.4 MB (51.67%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     bool isValid(string s) {
-      
       std::map<char, char> m {
         {'{', '}'}, {'(', ')'}, {'[', ']' },
         {'}', '{'}, {')', '('}, {']', '[' },
@@ -9,13 +8,10 @@ public:
       std::vector<char> open {'{', '[', '('};
       std::vector<char> stack;
       
-      if(s.length() < 2) return false;
-      
       for(auto i{0}; i < s.length(); ++i){
         auto rev = m[s[i]];
         
         if (std::find(open.begin(), open.end(), s[i]) != open.end()) {
-          std::cout << s[i] << endl;
           stack.push_back(s[i]);
         }else {
             if(stack.empty() || stack.back() != rev){
