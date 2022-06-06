d650ac618dba62beca48b865aa099b31b192a478
Miłosz Bieniek <bieniek.milosz0@gmail.com>
2022-06-06 15:57:25 +0200

    Time: 3 ms (45.43%), Space: 5.9 MB (25.42%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int countOdds(int low, int high) {
+      auto x = (high - low + 1);
+      
+      if( x & 1 && (high & 1 || low & 1)) return (x / 2 + 1);
+      return x/2;
+    }
+};
