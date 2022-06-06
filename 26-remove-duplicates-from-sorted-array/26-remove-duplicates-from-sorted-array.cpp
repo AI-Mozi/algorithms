15f5f0b3653be40c31bf94adb87ca1ef056d5c39
Miłosz Bieniek <bieniek.milosz0@gmail.com>
2022-06-06 12:38:21 +0200

    Time: 553 ms (7.52%), Space: 18.4 MB (36.80%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-      int counter{1};
       
       for(std::size_t i{1}; i < nums.size(); ++i){
-        std::cout << i << nums[i] << endl;
-        if(nums[i-1] > nums[i]) return counter;
+        //std::cout << i << nums[i] << endl
         
         if(nums[i] == nums[i-1]){
           // auto it = nums.begin() + i;
@@ -14,8 +12,7 @@ public:
           --i;
           continue;
         } 
-        ++counter;
       }
-      return counter;
+      return nums.size();
     }
 };
