3a9aff7f813ee7fbe4cfec95783a4b70b38097c1
Miłosz Bieniek <bieniek.milosz0@gmail.com>
2022-06-06 16:20:36 +0200

    Time: 4 ms (30.33%), Space: 6.3 MB (22.68%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        for(auto i{0}; i < haystack.length(); ++i){
-          //if(haystack[i] == needle[0]){
-            string sub = haystack.substr(i, needle.length());
-            if( sub == needle){
-              return i;
-            }
-          //}
-        }
+      for(auto i{0}; i < haystack.length(); ++i){
+        string sub = haystack.substr(i, needle.length());
+        if( sub == needle) return i;
+      }
       return -1;
     }
 };
