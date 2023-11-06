1b8cda2876c835da54f28cdfb29b0d403b03a97f
Miłosz Bieniek <bieniek.milosz0@gmail.com>
2023-11-06 14:16:58 +0100

    Time: 72 ms (48.29%), Space: 212.1 MB (52.67%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.rb
@@ -0,0 +1,16 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+  hash = {}
+  
+  nums.each_with_index do |n, idx|
+    t = target - n
+    
+    if hash.include?(t)
+      return [idx, hash[t]]
+    else
+      hash[n] = idx
+    end
+  end
+end
