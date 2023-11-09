5dbe6cd97ca0168527597b1e0bf927d65a3adfdc
Miłosz Bieniek <bieniek.milosz0@gmail.com>
2023-11-09 17:17:22 +0100

    Time: 110 ms (62.80%), Space: 211.6 MB (83.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.rb
+++ 0242-valid-anagram/0242-valid-anagram.rb
@@ -4,5 +4,16 @@
 def is_anagram(s, t)
   return false if s.size != t.size
 
-  return s.split('').sort.join('') == t.split('').sort.join('')
+  s_hash = {}
+  t_hash = {}
+  
+  s.each_char do |c|
+    s_hash[c] ? s_hash[c] += 1 : s_hash[c] = 0
+  end
+  
+  t.each_char do |c|
+    t_hash[c] ? t_hash[c] += 1 : t_hash[c] = 0
+  end
+  
+  return t_hash == s_hash
 end
