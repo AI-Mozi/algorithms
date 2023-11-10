f33e603d5e78860e7b1ef7618b58b2dd2c1dd9d1
Miłosz Bieniek <bieniek.milosz0@gmail.com>
2023-11-10 16:54:38 +0100

    Time: 80 ms (38.10%), Space: 212 MB (52.38%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.rb
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.rb
@@ -6,7 +6,7 @@ def top_k_frequent(nums, k)
   
   hash = {}
   nums.each do |n|
-    hash[n] ? hash[n] += 1 : hash[n] = 1
+    hash[n] = (hash[n] || 0) + 1
   end
   
   hash.sort_by { |k, v| -v }.to_h.keys.first(k)
