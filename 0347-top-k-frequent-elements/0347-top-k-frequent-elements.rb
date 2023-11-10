51b2985a8ec52ef96dc8a5b8a7d6b83d317d1915
Miłosz Bieniek <bieniek.milosz0@gmail.com>
2023-11-10 16:52:27 +0100

    Time: 86 ms (24.83%), Space: 211.9 MB (94.22%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.rb
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.rb
@@ -2,6 +2,8 @@
 # @param {Integer} k
 # @return {Integer[]}
 def top_k_frequent(nums, k)
+  return nums if k == nums.size
+  
   hash = {}
   nums.each do |n|
     hash[n] ? hash[n] += 1 : hash[n] = 1
