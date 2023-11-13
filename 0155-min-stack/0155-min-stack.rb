fafa61ada49305dead2b38a70f1700a5f8e1c429
Miłosz Bieniek <bieniek.milosz0@gmail.com>
2023-11-13 15:55:16 +0100

    Time: 80 ms (64.88%), Space: 213.8 MB (34.15%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.rb
@@ -0,0 +1,53 @@
+class MinStack
+    def initialize()
+      @stack = []
+      @min_stack = []
+    end
+
+=begin
+    :type val: Integer
+    :rtype: Void
+=end
+    def push(val)
+      if @min_stack.empty?
+        @min_stack << val
+      else
+        if val < get_min
+          @min_stack << val
+        else
+          @min_stack << get_min
+        end
+      end
+
+      @stack << val
+    end
+
+=begin
+    :rtype: Void
+=end
+    def pop()
+      @stack.pop
+      @min_stack.pop
+    end
+
+=begin
+    :rtype: Integer
+=end
+    def top()
+      @stack[-1]
+    end
+
+=begin
+    :rtype: Integer
+=end
+    def get_min()
+      @min_stack[-1]
+    end
+end
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack.new()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.get_min()
