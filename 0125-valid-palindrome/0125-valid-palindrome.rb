439042aeb92183c07648223e286ef83aec48a4e9
Miłosz Bieniek <bieniek.milosz0@gmail.com>
2023-11-20 13:12:27 +0100

    Time: 180 ms (9.17%), Space: 211.6 MB (76.20%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.rb
+++ 0125-valid-palindrome/0125-valid-palindrome.rb
@@ -1,16 +1,28 @@
 # @param {String} s
 # @return {Boolean}
 def is_palindrome(s)
-  s = s.gsub(/[^0-9a-z ]/i, '').downcase.delete(' ')
-  left_ptr = 0
-  right_ptr = s.size - 1
+  left = 0
+  right = s.size - 1
   
-  s.each_char do |char|
-    return false if s[left_ptr] != s[right_ptr]
+  while left < right
+    while left < right && !is_alpha(s[left])
+      left += 1
+    end
     
-    left_ptr += 1
-    right_ptr -= 1
+    while left < right && !is_alpha(s[right])
+      right -= 1
+    end
+    
+    return false if s[left].downcase != s[right].downcase
+    
+    left += 1
+    right -= 1
   end
   
   true
+end
+
+def is_alpha(c)
+  puts "#{c}: #{c.ord}"
+  (65..90).include?(c.ord) || (97..122).include?(c.ord) || (48..57).include?(c.ord) 
 end
