75744bb249c305299c542798eda25e50e52c2298
Miłosz Bieniek <bieniek.milosz0@gmail.com>
2024-04-13 18:46:21 +0200

    Time: 68 ms (36.26%), Space: 211.6 MB (9.93%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.rb
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.rb
@@ -10,23 +10,19 @@
 # @param {ListNode} list2
 # @return {ListNode}
 def merge_two_lists(list1, list2)
-  return nil if !list2 && !list1
-  return list1 unless list2
-  return list2 unless list1
-  
-  node = ListNode.new(nil)
+  node = ListNode.new
   head = node
 
   while (list1 && list2)
     if list1.val <= list2.val
       node.next = list1
-      node = list1
       list1 = list1.next
     else
       node.next = list2
-      node = list2
       list2 = list2.next
     end
+    
+    node = node.next
   end
   
   if list2
