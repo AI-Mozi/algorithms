9c98a4f9d0934977f4bb6f3b760a297061108622
Miłosz Bieniek <bieniek.milosz0@gmail.com>
2024-07-24 13:02:29 +0200

    Time: 149 ms (46.29%), Space: 221 MB (96.48%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.rb
@@ -0,0 +1,22 @@
+# @param {Integer[]} prices
+# @return {Integer}
+def max_profit(prices)
+  max_profit = 0
+  buy_day = 0
+  sell_day = buy_day + 1
+  
+  while sell_day <= prices.size - 1
+    if prices[buy_day] >= prices[sell_day]
+      buy_day = sell_day
+      sell_day += 1
+      next
+    end
+    
+    tmp_max_profit = prices[sell_day] - prices[buy_day]
+    
+    max_profit = [max_profit, tmp_max_profit].max
+    sell_day += 1
+  end
+  
+  max_profit
+end
