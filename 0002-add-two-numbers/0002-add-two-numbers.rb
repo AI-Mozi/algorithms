8664b59dbe328ba6ff538bd18e1116e64b4c684e
Miłosz Bieniek <bieniek.milosz0@gmail.com>
2024-08-10 21:19:31 +0200

    Time: 96 ms (22.46%), Space: 211.8 MB (11.91%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.rb
+++ 0002-add-two-numbers/0002-add-two-numbers.rb
@@ -13,55 +13,76 @@ def add_two_numbers(l1, l2)
   head = l1
   to_add = 0
   prev = nil
+  curr = nil
   
-  while l1 && l2
-    sum = l1.val + l2.val + to_add
-    to_add = 0
+#   while l1 && l2
+#     sum = l1.val + l2.val + to_add
+#     to_add = 0
     
-    if sum < 10
-      l1.val = sum 
-    else
-      l1.val = sum % 10
-      to_add += 1
-    end
+#     if sum < 10
+#       l1.val = sum 
+#     else
+#       l1.val = sum % 10
+#       to_add += 1
+#     end
     
-    prev = l1
+#     prev = l1
 
-    l1 = l1.next
-    l2 = l2.next
-  end
+#     l1 = l1.next
+#     l2 = l2.next
+#   end
   
-  while l1
-    sum = l1.val + to_add
+  while l1 || l2
+    sum = (l1&.val || 0) + (l2&.val || 0) + to_add
     to_add = 0
     
+    curr = l1 || l2
+    
     if sum < 10
-      l1.val = sum
+      curr.val = sum
     else
-      l1.val = sum % 10
+      curr.val = sum % 10
       to_add += 1
     end
     
-    prev = l1
-    l1 = l1.next
+    prev.next = curr if prev
+    prev = curr
+    
+    l1 = l1&.next
+    l2 = l2&.next
   end
   
-  while l2
-    prev.next = l2
+#   while l1
+#     sum = l1.val + to_add
+#     to_add = 0
     
-    sum = l2.val + to_add
-    to_add = 0
+#     if sum < 10
+#       l1.val = sum
+#     else
+#       l1.val = sum % 10
+#       to_add += 1
+#     end
     
-    if sum < 10
-      l2.val = sum
-    else
-      l2.val = sum % 10
-      to_add += 1
-    end
+#     prev = l1
+#     l1 = l1.next
+#   end
+  
+#   while l2
+#     prev.next = l2
     
-    prev = l2
-    l2 = l2.next
-  end
+#     sum = l2.val + to_add
+#     to_add = 0
+    
+#     if sum < 10
+#       l2.val = sum
+#     else
+#       l2.val = sum % 10
+#       to_add += 1
+#     end
+    
+#     prev = l2
+#     l2 = l2.next
+#   end
   
   if to_add > 0
     prev.next = ListNode.new(to_add)
